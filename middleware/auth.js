2184d07a37e1dcddede13e3e2f92c419a8460499
mustafashaikh4714 <shaikhmustafa.4714@gmail.com>
2019-04-27 02:10:41 +0530

    fix small issue

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,16 @@
-module.exports = redirectToLogin = (req, res, done) => {
+const redirectToLogin = (req, res, done) => {
   if (!req.session.userId) {
     res.redirect('/login')
   } else {
     done()
   }
 }
-module.exports = redirectToHome = (req, res, done) => {
+const redirectToHome = (req, res, done) => {
   if (req.session.userId) {
     res.redirect('/home')
   } else {
     done()
   }
 }
+
+module.exports = { redirectToHome, redirectToLogin }
